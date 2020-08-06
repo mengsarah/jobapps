885b3a6492c22dccac829b39d96a86bd1e37e184
Sarah Meng <shm3px@virginia.edu>
2020-08-06 19:43:16 -0400

    display forms (they don't do anything yet)

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+from .models import Company, JobApp, JobAppStep, Contact
+from .models import CompanyForm, JobAppForm, JobAppStepForm, ContactForm
+
 def index(request):
-    return render(request, "tracker/index.html")
+    companies = Company.objects
+    form_cp = CompanyForm()
+    form_ja = JobAppForm()
+    form_jas = JobAppStepForm()
+    form_ct = ContactForm()
+    forms = [form_cp, form_ja, form_jas, form_ct]
+    return render(request, "tracker/index.html", {'forms': forms})
