8016a660c571d70d884a2a171038caba9fdc98ab
Sarah Meng <shm3px@virginia.edu>
2020-08-06 19:42:59 -0400

    add models and ModelForms

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company, JobApp, JobAppStep, Contact
+
+admin.site.register(Company)
+admin.site.register(JobApp)
+admin.site.register(JobAppStep)
+admin.site.register(Contact)
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.forms import ModelForm
+import datetime
 
-# Create your models here.
+
+### CONSTANTS ###
+
+# actions are not necessarily in order or all required
+ACTIONS = [
+    ('talk', 'talked at event'),
+    ('resm', 'gave resume'),
+    ('covr', 'gave cover letter'),
+    ('appl', 'applied via job posting'),
+    ('chln', 'coding challenge/test'),
+    ('intv', 'interview')
+]
+
+ROLES = [
+    ('recr', 'recruiter'),
+    ('head', 'head of something'),
+    ('pers', 'personal contact'),
+    ('misc', 'other')
+]
+
+
+### MODELS FOR DATABASE AND FORMS ###
+
+class Company(models.Model):
+    name = models.CharField(max_length=75)
+    full_name = models.CharField(max_length=200)
+    about = models.TextField()
+    website = models.CharField(max_length=200, blank=True)
+    def __str__(self):
+        return self.name
+
+class CompanyForm(ModelForm):
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+
+class JobApp(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    position = models.CharField(max_length=200, blank=True)
+    resume = models.TextField(blank=True)
+    cover_letter = models.TextField(blank=True)
+    other_material = models.TextField(blank=True)
+    notes = models.TextField(blank=True)
+
+class JobAppForm(ModelForm):
+    class Meta:
+        model = JobApp
+        fields = '__all__'
+
+
+class JobAppStep(models.Model):
+    job_app = models.ForeignKey(JobApp, on_delete=models.CASCADE)
+    step = models.CharField(max_length=50, choices=ACTIONS, default='resm')
+    date = models.DateField(default=datetime.date.today())
+    done = models.BooleanField(default=True)
+    def __str__(self):
+        return self.step
+
+class JobAppStepForm(ModelForm):
+    class Meta:
+        model = JobAppStep
+        fields = '__all__'
+
+
+class Contact(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.TextField()
+    role = models.CharField(max_length=50, choices=ROLES, default='recr')
+    def __str__(self):
+        return self.name
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
