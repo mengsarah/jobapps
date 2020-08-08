f30feeadc5d6a0a449a48f3c740e4d5dfdfefd35
Sarah Meng <shm3px@virginia.edu>
2020-08-08 05:15:36 -0400

    enhance display of forms and info

--- tracker/views.py
+++ tracker/views.py
@@ -10,12 +10,18 @@ JobAppStepForm = modelform_factory(JobAppStep, fields='__all__')
 ContactForm = modelform_factory(Contact, fields='__all__')
 
 ### VIEWS ###
+### VIEWS FOR USERS ###
 
 def index(request):
-    companies = Company.objects
+    # blank forms
     form_cp = CompanyForm()
     form_ja = JobAppForm()
     form_jas = JobAppStepForm()
     form_ct = ContactForm()
-    forms = [form_cp, form_ja, form_jas, form_ct]
-    return render(request, "tracker/index.html", {'forms': forms})
+    forms = [
+        ('Company', form_cp, 'cp'),
+        ('Overall Job App', form_ja, 'ja'),
+        ('Job App Step', form_jas, 'jas'),
+        ('Contact', form_ct, 'ct')
+    ]
+    return render(request, "tracker/index.html", {'forms': forms, 'companies': Company.objects.all(),'steps': JobAppStep.objects.all()})
