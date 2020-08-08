14be2fc39afe0a1db9f8bf8b458b35b71b05be6b
Sarah Meng <shm3px@virginia.edu>
2020-08-08 17:38:04 -0400

    minor updates for display, cleaning, and clarification

--- tracker/models.py
+++ tracker/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.forms import ModelForm
 import datetime
 
 
@@ -38,10 +37,13 @@ class Company(models.Model):
 class JobApp(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     position = models.CharField(max_length=200, blank=True)
-    resume = models.TextField(blank=True)
+    # resume will just be the file name
+    resume = models.CharField(max_length=200)
     cover_letter = models.TextField(blank=True)
     other_material = models.TextField(blank=True)
     notes = models.TextField(blank=True)
+    def __str__(self):
+        return self.position + " at " + self.company.__str__()
 
 class JobAppStep(models.Model):
     job_app = models.ForeignKey(JobApp, on_delete=models.CASCADE)
@@ -49,11 +51,11 @@ class JobAppStep(models.Model):
     date = models.DateField(default=datetime.date.today())
     done = models.BooleanField(default=True)
     def __str__(self):
-        return self.step
+        return self.step + " for " + self.job_app.__str__()
 
 class Contact(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     role = models.CharField(max_length=50, choices=ROLES, default='recr')
     def __str__(self):
-        return self.name
+        return self.name + " at " + self.company.__str__()
