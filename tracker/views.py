355ae5ea7597859ebaef5ef71852af7e2b6985d5
Sarah Meng <shm3px@virginia.edu>
2020-08-11 01:44:16 -0400

    minor update/fix/enhance for edge cases and feedback messages

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.forms import modelform_factory
 from django.http import JsonResponse
+from django.contrib import messages
 
 from .models import Company, JobApp, JobAppStep, Contact
 
@@ -15,6 +16,8 @@ ContactForm = modelform_factory(Contact, fields='__all__')
 def get_contacts(request, company):
     if company:
         contacts = list(Contact.objects.filter(company=company).values())
+        if len(contacts) == 0:
+            contacts = "No contacts"
         return JsonResponse({'contacts': contacts})
 
 ### VIEWS FOR POST ###
@@ -22,12 +25,14 @@ def get_contacts(request, company):
 def add_new(request):
     if request.method == 'POST':
         data = request.POST
+        added = ""
         if data['form-type'] == 'type-cp':
             new_company = Company()
             new_company.name = data['name']
             new_company.full_name = data['full_name']
             new_company.about = data['about']
             new_company.website = data['website']
+            added = "company: " + new_company.__str__()
             new_company.save()
         elif data['form-type'] == 'type-ja':
             new_job_app = JobApp(company_id = data['company'])
@@ -36,12 +41,14 @@ def add_new(request):
             new_job_app.cover_letter = data['cover_letter']
             new_job_app.other_material = data['other_material']
             new_job_app.notes = data['notes']
+            added = "job app: " + new_job_app.__str__()
             new_job_app.save()
         elif data['form-type'] == 'type-jas':
             new_job_app_step = JobAppStep(job_app_id = data['job_app'])
             new_job_app_step.step = data['step']
             new_job_app_step.date = data['date']
             new_job_app_step.done = bool(data['done'])
+            added = "job app step: " + new_job_app_step.__str__()
             new_job_app_step.save()
         elif data['form-type'] == 'type-ct':
             new_contact = Contact(company_id = data['company'])
@@ -49,8 +56,13 @@ def add_new(request):
             new_contact.role = data['role']
             new_contact.email = data['email']
             new_contact.phone = data['phone']
+            added = "contact: " + new_contact.__str__()
             new_contact.save()
+        messages.success(request, "Added " + added)
         return redirect("tracker:index")
+    # else (not a POST request)
+    messages.error(request, "No data received")
+    return redirect("tracker:index")
 
 ### VIEWS FOR USERS ###
 
